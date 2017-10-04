08b2aeb0afee927a6a522532b5716740e5126bc2
zhujiang <zhujiangmail@hotmail.com>
2017-10-04 14:11:56 +0800

    bpcv

--- hmprjs/create_gray_lmdb.cmd
+++
@@ -1,8 +0,0 @@
-set  TOOLS=c:\mingw\bin
-set  IMGSDIR=d:\caffe_net\images
-
-%TOOLS%\convert_imageset.exe  --gray  --resize_height=115  --resize_width=115  %IMGSDIR%\places_train\  .\data\train.txt  .\train_lmdb
-
-%TOOLS%\convert_imageset.exe  --gray  --resize_height=115  --resize_width=115  %IMGSDIR%\places_test\   .\data\val.txt    .\test_lmdb
-
-
